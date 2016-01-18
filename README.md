589908c851262b623a355d065dd29f4d03bd7026
Tony Crowe <github@tonycrowe.com>
2016-01-18 12:53:08 -0700

    update yeoman tests

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 
 # hexo theme generator for yeoman
----
 It's used to quickly scaffold a theme for hexo. The theme it creates is fully functional and has no design. That part is up to you!
 
 ### Features
----
 It creates all of the necessary files you need to make your own theme.
 
 You will be prompted for:
@@ -23,26 +21,35 @@ You will be prompted for:
 + Bower? (yes/no)
 
 ### Install
----
 ```sh
 npm install -g generator-hexo-theme
 ```
 
 ### Use
----
 Navigate to the directory you want to place the theme project in.
 ```sh
 mkdir mytheme
 cd mytheme
 yo hexo-theme
 ```
-Then goto your hexo site project's `_config.yml` and switch the `theme` propery to what you have named it.
+
++ Check main blog `_config.yml`
+  * Set theme property to your theme name
++ Check theme-specific `_config.yml`
+  * Change menu items if needed
+  * Change stylesheet and js list if needed
 
 ### Ideas
 Please post any ideas to issues. If you know how to use any of the technologies it generates better than me there are places, especially in the templates, where it could be improved by you! Pull requests welcome.
 
-### Resources
----
+### Hexo renderers
+It might be necessary to goto the blog root and install a specific renderer for the template language you have chosen. Swig is built into Hexo at the current version.
+```
+npm install hexo-renderer-jade
+npm install hexo-renderer-ejs
+```
+
+### Resources -- What is all this for?
 + hexo static site generator [hexo.io](http://hexo.io)
 + Yeoman scaffolding tool [yeoman.io](http://yeoman.io)
 + Bower package manager [bower.io](http://bower.io)
--- test/test-creation.js
+++ test/test-creation.js
@@ -1,87 +1,38 @@
-/*global describe, beforeEach, it */
 'use strict';
-var path = require('path');
-var helpers = require('yeoman-generator').test;
 
-/*
-	Most of this is auto-generated from the generator-generator.
+var path = require('path'),
+    helpers = require('yeoman-test'),
+    assert = require('yeoman-assert'),
+    defaultOptions;
 
-	We are rolling with this for now. If you think of a better way
-	you are welcome to refactor this.
-*/
+defaultOptions = {
+    themeName: 'temp-theme',
+    templateLang: 'ejs',
+    stylesheetLang: 'styl',
+    technicalFeatures: [
+        'scripts',
+        'bower'
+    ]
+};
 
-describe('hexo-theme generator', function() {
-	beforeEach(function(done) {
-		helpers.testDirectory(path.join(__dirname, 'temp'), function(err) {
-			if (err) {
-				return done(err);
-			}
-			this.app = helpers.createGenerator('hexo-theme:app', [
-				'../../app'
-			]);
-			done();
-		}.bind(this));
-	});
+describe('generator-hexo-theme', function () {
+    beforeEach(function (done) {
+        helpers.run(path.join(__dirname, '..', 'app'))
+            .withOptions(defaultOptions)
+            .withPrompts(defaultOptions)
+            .on('end', done);
+    });
 
-	// run through EJS theme creation
-	it('should be able to generate an ejs theme', function(done) {
-		helpers.mockPrompt(this.app, {
-			themename: 'test-theme-ejs',
-			templatelang: 'ejs',
-			features: [
-				'scripts',
-				'bower',
-				'editorconfig',
-				'jshint'
-			]
-		});
-
-		// what is this?
-		this.app.options['skip-install'] = true;
-
-		// check for the files we expect to be created
-		this.app.run({}, function() {
-			helpers.assertFile([
-				'layout',
-				'scripts',
-				'source',
-				'.editorconfig',
-				'.jshintrc',
-				'_config.yml',
-				'bower.json',
-				'.bowerrc',
-				'package.json'
-			]);
-			done();
-		});
-	});
-
-	it('should be able to generate a jade theme', function(done) {
-		helpers.mockPrompt(this.app, {
-			themename: 'test-theme-jade',
-			templatelang: 'jade',
-			features: [
-				'scripts',
-				'bower',
-				'editorconfig',
-				'jshint'
-			]
-		});
-		this.app.options['skip-install'] = true;
-		this.app.run({}, function() {
-			helpers.assertFile([
-				'layout',
-				'scripts',
-				'source',
-				'.editorconfig',
-				'.jshintrc',
-				'_config.yml',
-				'bower.json',
-				'.bowerrc',
-				'package.json'
-			]);
-			done();
-		});
-	});
-
+    it('can make the theme', function () {
+        assert.file([
+            '.bowerrc',
+            'bower.json',
+            '_config.yml',
+            'package.json',
+            'source/favicon.ico',
+            'source/css/temp-theme.styl',
+            'source/js/temp-theme.js',
+            'layout/index.ejs'
+        ]);
+    });
 });
